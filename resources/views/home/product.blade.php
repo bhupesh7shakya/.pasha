014035df09d5b27593920f8fc2c9bbd1437e5b63
bhupesh shakya <bhupesh7shakya@gmail.com>
2022-09-03 18:57:19 +0545

    rating system

--- resources/views/home/product.blade.php
+++ resources/views/home/product.blade.php
@@ -1,7 +1,7 @@
 @extends('home.layouts.app')
 @section('custom-style')
     <style>
-    .rating-list li {
+    /* .rating-list li {
     float: right;
     color: #ddd;
     padding: 10px 5px;
@@ -15,7 +15,58 @@
     .rating-list {
     display: inline-block;
     list-style: none;
-    }
+    } */
+    fieldset, label { margin: 0; padding: 0; }
+body{ margin: 20px; }
+h1 { font-size: 1.5em; margin: 10px; }
+
+/****** Style Star Rating Widget *****/
+    .rating {
+  border: none;
+  float: left;
+}
+
+.rating > input { display: none; }
+.rating > label:before {
+  margin: 5px;
+  font-size: 1.25em;
+  font-family: FontAwesome;
+  display: inline-block;
+  content: "\f005";
+}
+
+.rating > .half:before {
+  content: "\f089";
+  position: absolute;
+}
+
+.rating > label {
+  color: #ddd;
+ float: right;
+}
+
+/***** CSS Magic to Highlight Stars on Hover *****/
+
+.rating > input:checked ~ label, /* show gold star when clicked */
+.rating:not(:checked) > label:hover, /* hover current star */
+.rating:not(:checked) > label:hover ~ label { color: #FFD700;  } /* hover previous stars in list */
+
+.rating > input:checked + label:hover, /* hover current star when changing rating */
+.rating > input:checked ~ label:hover,
+.rating > label:hover ~ input:checked ~ label, /* lighten current selection */
+.rating > input:checked ~ label:hover ~ label { color: #FFED85;  }
+<fieldset class="rating">
+    <input type="radio" id="star5" name="rating" value="5" /><label class = "full" for="star5" title="Awesome - 5 stars"></label>
+    <input type="radio" id="star4half" name="rating" value="4 and a half" /><label class="half" for="star4half" title="Pretty good - 4.5 stars"></label>
+    <input type="radio" id="star4" name="rating" value="4" /><label class = "full" for="star4" title="Pretty good - 4 stars"></label>
+    <input type="radio" id="star3half" name="rating" value="3 and a half" /><label class="half" for="star3half" title="Meh - 3.5 stars"></label>
+    <input type="radio" id="star3" name="rating" value="3" /><label class = "full" for="star3" title="Meh - 3 stars"></label>
+    <input type="radio" id="star2half" name="rating" value="2 and a half" /><label class="half" for="star2half" title="Kinda bad - 2.5 stars"></label>
+    <input type="radio" id="star2" name="rating" value="2" /><label class = "full" for="star2" title="Kinda bad - 2 stars"></label>
+    <input type="radio" id="star1half" name="rating" value="1 and a half" /><label class="half" for="star1half" title="Meh - 1.5 stars"></label>
+    <input type="radio" id="star1" name="rating" value="1" /><label class = "full" for="star1" title="Sucks big time - 1 star"></label>
+    <input type="radio" id="starhalf" name="rating" value="half" /><label class="half" for="starhalf" title="Sucks big time - 0.5 stars"></label>
+</fieldset>
         </style>
 @endsection
 @section('home-content')
@@ -107,16 +158,35 @@
         @auth
             <form action="{{route('review.store')}}" method="post" class="my-5">
                 @csrf
-                <input type="hidden" id="rating" name="rating" value="0">
+                {{--
                 <ul class="list-inline d-flex rating-list">
                     <li><i class="fa fa-star" onclick="rate(5)" title="Rate 5"></i></li>
                     <li><i class="fa fa-star" onclick="rate(4)" title="Rate 4"></i></li>
                     <li><i class="fa fa-star" onclick="rate(3)" title="Rate 3"></i></li>
                     <li><i class="fa fa-star" onclick="rate(2)" title="Rate 2"></i></li>
                     <li><i class="fa fa-star" onclick="rate(1)" title="Rate 1"></i></li>
-                  </ul>
+                  </ul> --}}
+                  <input type="hidden" id="rating" name="rating" value="0">
+                  @error('rating')
+                    <p class="text-red">{{$message}}</p>
+                @enderror
+                  <fieldset class="rating">
+                    <input type="radio" onclick="rate(5)" id="star5"  value="5" /><label class = "full" for="star5" title="Awesome - 5 stars"></label>
+                    <input type="radio" onclick="rate(4.5)" id="star4half" value="4 and a half" /><label class="half" for="star4half" title="Pretty good - 4.5 stars"></label>
+                    <input type="radio" onclick="rate(4)" id="star4"  value="4" /><label class = "full" for="star4" title="Pretty good - 4 stars"></label>
+                    <input type="radio" onclick="rate(3.5)" id="star3half"  value="3 and a half" /><label class="half" for="star3half" title="Meh - 3.5 stars"></label>
+                    <input type="radio" onclick="rate(3)" id="star3"  value="3" /><label class = "full" for="star3" title="Meh - 3 stars"></label>
+                    <input type="radio" onclick="rate(2.5)" id="star2half"  value="2 and a half" /><label class="half" for="star2half" title="Kinda bad - 2.5 stars"></label>
+                    <input type="radio" onclick="rate(2)" id="star2"  value="2" /><label class = "full" for="star2" title="Kinda bad - 2 stars"></label>
+                    <input type="radio" onclick="rate(1.5)" id="star1half"  value="1 and a half" /><label class="half" for="star1half" title="Meh - 1.5 stars"></label>
+                    <input type="radio" onclick="rate(1)" id="star1"  value="1" /><label class = "full" for="star1" title="Sucks big time - 1 star"></label>
+                    <input type="radio" onclick="rate(0.5)"  id="starhalf"  value="half" /><label class="half" for="starhalf" title="Sucks big time - 0.5 stars"></label>
+                </fieldset>
                 <input type="hidden" name="product_id" value="{{$data['product']->id}}">
                 <textarea name="comment" id="" cols="30" rows="2" class="form-control "></textarea>
+                @error('comment')
+                    <p class="text-red">{{$message}}</p>
+                @enderror
                 <br>
                 <button type="submit" class="btn btn-primary float-end">save</button>
             </form>
