1d56230a692d74c16eefa92b7baa3a02bec2212b
bhupesh shakya <bhupesh7shakya@gmail.com>
2023-01-07 19:17:28 +0545

    updated deploy

--- deploy.php
+++ deploy.php
@@ -16,8 +16,8 @@ add('writable_dirs', []);
 
 host('13.126.65.159')
     ->set('remote_user', 'ubuntu')
-    ->set('deploy_path', '/var/www');
-
+    ->set('deploy_path', '/var/www')
+    ->set('stage', 'production');
 // Hooks
 
 after('deploy:failed', 'deploy:unlock');
