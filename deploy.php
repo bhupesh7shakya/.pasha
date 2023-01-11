83d32ade2decdc8a173eedd040e2b4316ce44cb1
bhupesh shakya <bhupesh7shakya@gmail.com>
2023-01-11 09:57:09 +0545

    basic deploy

--- deploy.php
+++ deploy.php
@@ -8,16 +8,18 @@ require 'recipe/laravel.php';
 
 set('repository', 'https://github.com/bhupesh7shakya/.pasha.git');
 
-add('shared_files', []);
-add('shared_dirs', []);
-add('writable_dirs', []);
+// add('shared_files', []);
+// add('shared_dirs', []);
+// add('writable_dirs', []);
 
 // Hosts
 
-host('13.126.65.159')
+host('13.235.134.158')
     ->set('remote_user', 'ubuntu')
-    ->set('deploy_path', '/var/www')
-    ->set('stage', 'production');
+    ->set('deploy_path', '/home');
+// task
+task('my_task', function () {
+    run('whoami');
+});
 // Hooks
-
 after('deploy:failed', 'deploy:unlock');
